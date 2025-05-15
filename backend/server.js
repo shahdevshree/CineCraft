36bbb7d320344afd90d0d17ff104f6cac3d3c3e5
Devshree <shahdevshree03@gmail.com>
2025-05-15 16:57:25 -0400

    Solved errors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import path from "path";
 const app = express();
 const PORT = ENV_VARS.PORT
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,13 +22,13 @@ app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
-if (ENV_VARS.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (ENV_VARS.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
 
 app.listen(PORT, () => {
 	console.log("Server started at http://localhost:"+PORT);
